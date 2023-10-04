1e74db18eefea9ce093a7d601ca7dc4447e5a74c
M Nu'man Arif H <numan.arif@ikonsultan.com>
2023-10-04 09:59:17 +0700

    Setup Docker and Environment API

---
+++ custom_addons/ikon_recruitment/__init__.py
@@ -0,0 +1,4 @@
+from . import controller
+from . import security
+from . import models
+from . import views
---
+++ custom_addons/ikon_recruitment/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'IKON Recruitment Module',
+    'summary': """IKON Rec Module""",
+    'version': '16.0',
+    "author": "Ikon Developer",
+    'company': 'Ikonsultan Inovatama',
+    'website': 'https://www.ikonsultan.com',
+    'category': 'Tools',
+    'depends': ['base', 'ppu_hr'],
+    "external_dependencies": {"python3.9": ["graphene"]},
+    'license': 'AGPL-3',
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
